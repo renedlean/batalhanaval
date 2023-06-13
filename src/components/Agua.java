8dde65532f53a249889a3fe27a8ba6d99ea94541
renedlean <31704907+renedlean@users.noreply.github.com>
2023-06-12 22:15:52 -0300

    teste

--- src/components/Agua.java
+++
@@ -1,7 +0,0 @@
-package components;
-
-public class Agua extends  Posicao{
-    Agua(int linhaInicial, int colunaInicial) {
-        super("Agua", linhaInicial, colunaInicial, 'A');
-    }
-}
--- src/components/PortaAviao.java
+++
@@ -1,7 +0,0 @@
-package components;
-
-class PortaAviao extends Posicao {
-     PortaAviao(int linhaInicial, int colunaInicial) {
-        super("PortaAvião", linhaInicial, colunaInicial, 'P');
-    }
-}
